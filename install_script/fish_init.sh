f1e230af9518749d9154dc458799e99c75bc6af4
S-Ihara <shun27182@gmail.com>
2023-04-15 02:56:32 +0900

    update

--- install_script/fish_init.sh
+++ install_script/fish_init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-
 # config files
 cd ~/dotfiles
 ln -si ./fisher.fish $HOME/.config/fish/functions/fisher.fish
